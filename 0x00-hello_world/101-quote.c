8f77fb33b6c362b0269ca8dbc1673957c79c9b60
modernfuthark <timothys1738@gmail.com>
2020-02-05 12:29:28 -0500

    Fixed character count in first write statement

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 57);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
 	write(2, "\n", 1);
 
 	return (1);
