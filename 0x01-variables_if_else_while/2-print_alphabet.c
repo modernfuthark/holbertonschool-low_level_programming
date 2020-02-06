a524bdc67d86f18ab336d710a2dc21fada2da7db
modernfuthark <timothys1738@gmail.com>
2020-02-06 09:12:07 -0500

    Updated file with answer to task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Main function
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char alpha;
+	
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
