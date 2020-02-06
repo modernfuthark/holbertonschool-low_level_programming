28448e9b542646a1fd01bf88298163dd398943f6
modernfuthark <timothys1738@gmail.com>
2020-02-06 09:13:55 -0500

    Created file and filled with code

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Main function
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char alpha;
+	
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+		putchar(alpha);
+
+
+	putchar('\n');
+
+	return (0);
+}
