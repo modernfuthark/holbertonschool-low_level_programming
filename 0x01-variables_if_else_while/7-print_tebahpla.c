1bca51e2c683270d1523a8df9c6001fedef56b12
modernfuthark <timothys1738@gmail.com>
2020-02-06 09:28:09 -0500

    Created file, filled with code

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char ahpla;
+
+	for (ahpla = 'z'; ahpla >= 'a'; ahpla--)
+		putchar(ahpla);
+
+	putchar('\n');
+
+	return (0);
+}
