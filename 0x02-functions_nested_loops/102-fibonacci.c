302bf8af77fa6592250a8f48bbb6dc4225273c3c
modernfuthark <timothys1738@gmail.com>
2020-02-11 10:48:55 -0500

    Fixed starting numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * main - Main function
  *
- *
- *
+ * Return: 0 on sucess
  */
 
 int main(void)
 {
 	unsigned long int first = 1;
-	unsigned long int second = 2;
+	unsigned long int second = 0;
 	unsigned long int result = 0;
 	int i;
 
-	for ( i = 0; i < 50; i++)
+	for (i = 0; i < 50; i++)
 	{
 		result = first + second;
 		printf("%ld", result);
