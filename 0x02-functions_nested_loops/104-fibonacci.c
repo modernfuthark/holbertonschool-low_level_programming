f3daa82843d423716cfc536350d60832507ed5b8
modernfuthark <timothys1738@gmail.com>
2020-02-11 11:11:22 -0500

    Changed data type

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: 0 on sucess
+ */
+
+int main(void)
+{
+	long double first = 0;
+	long double second = 1;
+	long double result = 0;
+	int i;
+
+	for (i = 0; i < 98; i++)
+	{
+		result = first + second;
+		printf("%.0Lf", result);
+		if (i != 97)
+			printf(", ");
+		first = second;
+		second = result;
+	}
+	printf("\n");
+
+	return (0);
+}
