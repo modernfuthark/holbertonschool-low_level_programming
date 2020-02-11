0c252f3983824335948e2d19717fde5034045a54
modernfuthark <timothys1738@gmail.com>
2020-02-11 13:33:28 -0500

    Created file and added answer

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Print all natural numbers <1024, then add
+ *
+ * Return: 1 on success
+ */
+
+int main(void)
+{
+	int sum, i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (1);
+}
