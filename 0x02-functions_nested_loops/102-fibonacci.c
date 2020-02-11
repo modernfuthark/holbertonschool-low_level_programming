f8703e1c5c85d4fca56efc6d55aaafde6bbc79f0
modernfuthark <timothys1738@gmail.com>
2020-02-11 10:23:34 -0500

    Updated file for advanced task 3 (14)

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	unsigned long int first = 1;
+	unsigned long int second = 2;
+	unsigned long int result = 0;
+	int i;
+
+	for ( i = 0; i < 50; i++)
+	{
+		result = first + second;
+		printf("%ld", result);
+		if (i != 49)
+			printf(", ");
+		first = second;
+		second = result;
+	}
+	printf("\n");
+
+	return (0);
+}
