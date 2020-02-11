84f71f31cfe48f5de5c4988737c5fd52eb59d2c9
modernfuthark <timothys1738@gmail.com>
2020-02-11 13:39:34 -0500

    Fixed loop

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,11 +14,11 @@ int main(void)
 	long double result = 0;
 	int i;
 
-	for (i = 0; i < 99; i++)
+	for (i = 0; i < 98; i++)
 	{
 		result = first + second;
 		printf("%.0Lf", result);
-		if (i != 98)
+		if (i != 97)
 			printf(", ");
 		first = second;
 		second = result;
