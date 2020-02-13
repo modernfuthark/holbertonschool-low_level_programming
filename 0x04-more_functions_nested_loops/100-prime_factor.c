d449685d8a1fc37d8a235aa4eca79793ef66d9c4
modernfuthark <timothys1738@gmail.com>
2020-02-13 14:29:06 -0500

    Created file and inserted code

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/**
+ * main - Print largest prime factor of a hardcoded number
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int i;
+
+	while (n % 2 == 0)
+	{
+		n = n / 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			n = n / i;
+		}
+	}
+
+	if (n > 2)
+		printf("%ld ", n);
+
+	printf("\n");
+	return (0);
+}
