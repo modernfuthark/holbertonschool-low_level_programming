66b6a36ef23331cee835efff3c2964cd584fd2d6
modernfuthark <timothys1738@gmail.com>
2020-02-18 10:21:59 -0500

    Fixed logic with length and loop

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,19 +8,26 @@
 void puts_half(char *str)
 {
 	int len = 0;
+	int i;
 
 	while (str[len] != '\0')
 		len++;
 
 
 	if (len % 2 == 0)
+	{
 		len = len / 2;
+		i = len;
+	}
 	else
+	{
 		len = (len - 1) / 2;
+		i = len + 1;
+	}
 
 
-	for (; str[len] != '\0'; len++)
-		_putchar(str[len]);
+	for (; str[i] != '\0'; i++)
+		_putchar(str[i]);
 
 	_putchar('\n');
 }
