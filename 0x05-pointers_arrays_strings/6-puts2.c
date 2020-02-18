b485dd3370836f503b976aa1d0c81226840a6640
modernfuthark <timothys1738@gmail.com>
2020-02-18 10:03:58 -0500

    Added edge case checking if str exists

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,12 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	if (str)
 	{
-		_putchar(str[i]);
+		for (i = 0; str[i] != '\0'; i += 2)
+		{
+			_putchar(str[i]);
+		}
 	}
 
 	_putchar('\n');
