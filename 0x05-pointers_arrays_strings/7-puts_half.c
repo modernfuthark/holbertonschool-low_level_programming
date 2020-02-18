6559186e156cfbe58898622c3a57c279a5b36e7c
modernfuthark <timothys1738@gmail.com>
2020-02-18 10:13:57 -0500

    Updated code, changed len

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,19 +8,19 @@
 void puts_half(char *str)
 {
 	int len = 0;
-	int i;
 
 	while (str[len] != '\0')
 		len++;
 
-	if (len % 2 != 0)
-		len = (len - 1) / 2;
-	else
+
+	if (len % 2 == 0)
 		len = len / 2;
+	else
+		len = (len - 1) / 2;
 
 
-	for ( i = 0; i <= len; i++)
-		_putchar(str[i]);
+	for (; str[len] != '\0'; len++)
+		_putchar(str[len]);
 
 	_putchar('\n');
 }
