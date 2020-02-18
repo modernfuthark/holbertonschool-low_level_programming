876392b19cf470ac23320f9763c70c3f66617588
modernfuthark <timothys1738@gmail.com>
2020-02-18 10:00:46 -0500

    Added edge case for if n = 0

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,16 +12,14 @@ void print_array(int *a, int n)
 	int i;
 	int checky;
 
-	if (n == 0)
-		checky = 0;
-	else
-		checky = n - 1;
-
-	for (i = 0; i <= checky; i++)
+	if (n != 0)
 	{
-		printf("%d", a[i]);
-		if (i != checky)
-			printf(", ");
+		for (i = 0; i <= checky; i++)
+		{
+			printf("%d", a[i]);
+			if (i != checky)
+				printf(", ");
+		}
 	}
 	printf("\n");
 }
