5a24d5b2a31d729eb025a13ce66a135bd318ed6e
modernfuthark <timothys1738@gmail.com>
2020-02-20 12:22:19 -0500

    Updated edge case

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,21 +12,18 @@ int _strcmp(char *s1, char *s2)
 	int s2Len = 0;
 	int i;
 
-	if(s1 && s2)
-	{
-		while (s1[++s1Len])
-			;
+	while (s1[++s1Len])
+		;
 
-		while (s2[++s2Len])
-			;
+	while (s2[++s2Len])
+		;
 
-		for (i = 0; s1[i] == s2[i]; i++)
-			if (s1[i])
-				return (0);
-	}
+	for (i = 0; s1[i] == s2[i]; i++)
+		if (s1[i])
+			return (0);
 
-		if (s1Len < s2Len)
-			return (s1[i] - s2[i]); /*s1 smaller than s2*/
-		else
-			return (s1[i] - s2[i]); /*s1 is bigger than s2*/
+	if (s1Len < s2Len)
+		return (s1[i] - s2[i]); /*s1 smaller than s2*/
+	else
+		return (s1[i] - s2[i]); /*s1 is bigger than s2*/
 }
