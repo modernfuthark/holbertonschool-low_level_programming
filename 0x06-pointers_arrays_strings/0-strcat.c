b57524b424b347c3bbfe0eeb5fd7c937f91518a8
modernfuthark <timothys1738@gmail.com>
2020-02-20 09:28:15 -0500

    Created file and inserted code

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strcat - Concatenates 2 strings
+ * @dest: Destination (to)
+ * @src: Source (from)
+ * Return: Pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int desLen = 0;
+
+	while(dest[++desLen])
+		;
+
+	for(i = 0; src[i]; i++)
+	{
+		dest[desLen + i] = src[i];
+	}
+	dest[desLen + i] = '\0';
+
+	return (dest);
+}
