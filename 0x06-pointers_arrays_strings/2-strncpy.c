a4c054a98b3395463e115fb0f5afb76502d78371
modernfuthark <timothys1738@gmail.com>
2020-02-20 12:16:32 -0500

    Updated how the null byte gets added

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,8 +18,12 @@ char *_strncpy(char *dest, char *src, int n)
 			dest[i] = src[i];
 			i++;
 		}
-		if (src && i < n)
+
+		while (i < n)
+		{
 			dest[i] = '\0';
+			i++;
+		}
 	}
 
 	return (dest);
