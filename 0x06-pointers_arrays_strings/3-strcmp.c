8c83bbdfa8ad9c43879a694e9c5ad5e26c67360d
modernfuthark <timothys1738@gmail.com>
2020-02-20 12:44:10 -0500

    Rewrote loop, instead using a comparison to check for the same ascii rather than doing it inside the loop

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,23 +10,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int s1Len = 0;
-	int s2Len = 0;
-	int i;
+	int i = 0;
 
-	if (!(s1) || !(s2))
-		return (0);
-
-
-	while (s1[++s1Len])
-		;
-
-	while (s2[++s2Len])
-		;
-
-	for (i = 0; s1[i] == s2[i]; i++)
-		if (s1[i])
-			return (0);
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
 
 	return (s1[i] - s2[i]);
 }
