862fdf25e183254e9fed84638ab1abe243485195
modernfuthark <timothys1738@gmail.com>
2020-02-20 12:34:32 -0500

    Updated return

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,9 @@
  * _strcmp - Compare 2 strings
  * @s1: String 1
  * @s2: String 2
- * Return: idfk lol
+ * Return: 0 if the same
+ * > 0 if s1 is smaller than s2
+ * < 0 if s1 is bigger than s1
  */
 
 int _strcmp(char *s1, char *s2)
@@ -11,6 +13,10 @@ int _strcmp(char *s1, char *s2)
 	int s1Len = 0;
 	int s2Len = 0;
 	int i;
+
+	if (!(s1) || !(s2))
+		return (0);
+
 
 	while (s1[++s1Len])
 		;
@@ -22,8 +28,5 @@ int _strcmp(char *s1, char *s2)
 		if (s1[i])
 			return (0);
 
-	if (s1Len < s2Len)
-		return (s1[i] - s2[i]); /*s1 smaller than s2*/
-	else
-		return (s1[i] - s2[i]); /*s1 is bigger than s2*/
+	return (s1[i] - s2[i]);
 }
