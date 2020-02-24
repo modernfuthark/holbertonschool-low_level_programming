a4ff3a7d12a71aef031ea8dd9b4c96dab6302c4c
modernfuthark <timothys1738@gmail.com>
2020-02-24 10:37:02 -0500

    Created file and inserted code

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strchr - Locate a char in string s
+ * @s: String
+ * @c: Star to find in s
+ * Return: Pointer to c in s, NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (; s; s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+
+	return (NULL);
+}
