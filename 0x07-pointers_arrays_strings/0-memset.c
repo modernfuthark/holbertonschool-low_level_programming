6a2d5426747c989d1b7b281e75f3a54e06702609
modernfuthark <timothys1738@gmail.com>
2020-02-24 10:07:06 -0500

    Created file and inserted code

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _memset - Set n bytes of s to b
+ * @s: Memory to mod
+ * @b: Char to fill n bytes of s
+ * @n: Amount to fill
+ * Return: Pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	n--; /*Count like a computer!*/
+
+	for (i = 0; i <= n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
