f423b45ea1f41517caad1f4f0f8b7cfed44d7990
modernfuthark <timothys1738@gmail.com>
2020-02-24 11:00:44 -0500

    Created file and inserted code

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strspn - Returns len for every instance of accept found in s
+ * @s: String
+ * @accept: Filter
+ * Return: Number of instances of accept found in s (len)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, len = 0;
+
+	for (i = 0; s[i] && s[i] != ' '; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				len++;
+				break;
+			}
+		}
+	}
+	return (len);
+}
