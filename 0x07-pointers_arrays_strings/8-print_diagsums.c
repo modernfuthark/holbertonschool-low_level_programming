0a06ab5afdf4583371749a4db058242adf6df9b8
modernfuthark <timothys1738@gmail.com>
2020-02-24 15:04:13 -0500

    Attempted problem

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - Print diagonal sums
+ * @a: Matrix
+ * @size: Size of matrix
+ * Return: Nothing, function is void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	/*int i = 0, j = 0, counter = 0, revCount = size - 1, sumFr = 0, sumBk = 0;*/
+
+
+	printf("%ls\n%d\n", a, size);
+	/*while (i < size)
+	{
+		while (j < size)
+		{
+			if (j == counter)
+				sumFr += a[i];
+			if (j == revCount)
+				sumBk += a[i];
+			j++;
+		}
+		j = 0;
+		counter++;
+		revCount--;
+		i++;
+	}
+
+	printf("%d, %d\n", sumFr, sumBk);*/
+}
