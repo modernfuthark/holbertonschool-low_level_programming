538f8fb653acb1b6448365345d60ac70d09178c1
modernfuthark <timothys1738@gmail.com>
2020-02-29 17:55:33 -0500

    Updated betty, added attribute unused to argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * main - Main function
+ * @argc: Unused
+ * @argv: Given arguments
+ * Return: 0 on success
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
