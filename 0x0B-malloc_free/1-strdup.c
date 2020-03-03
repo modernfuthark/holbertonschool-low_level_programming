f0fb6f6ed83069118c545a451aecb0e03676ef45
modernfuthark <timothys1738@gmail.com>
2020-03-03 11:00:09 -0500

    Created file and inserted file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _strdup - Duplicates a string
+ * @str: String
+ * Return: Pointer to new string, NULL if malloc failed
+ */
+
+char *_strdup(char *str)
+{
+	char *copy;
+	int i, len = 0;
+
+	if (!str)
+		return (NULL);
+
+	while (str[++len])
+		;
+
+	copy = malloc(sizeof(char *) * len);
+	if (!copy)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		copy[i] = str[i];
+	}
+
+	copy[i] = '\0';
+	return (copy);
+}
