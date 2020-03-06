52cd2c4c4f1e3b8181ee790c088b682649fd5d82
modernfuthark <timothys1738@gmail.com>
2020-03-05 20:22:56 -0500

    Created file and inserted code

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+/**
+ * string_nconcat - Concat 2 strings by n bytes
+ * @s1: String 1
+ * @s2: String 2
+ * @n: Size
+ * Return: Pointer to new string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1 = 0, len2 = 0, i, j;
+	char *result;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	if (n <= 0)
+		return (NULL);
+
+	n--; /*Count like a computer, please*/
+	while (s1[len1])
+		++len1;
+	while (s2[len2])
+		++len2;
+	if (n >= len2)
+		n = len2;
+
+	result = malloc(sizeof(char) * (len1 + n));
+	if (!result)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)
+	{
+		result[i] = s1[i];
+	}
+	for (j = 0; j <= n; j++)
+	{
+		result[i + j] = s2[j];
+	}
+	result[i + j] = '\0';
+
+	return (result);
+}
