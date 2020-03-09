a0379ab12474ee8ad991e42b757914af7ee0d36c
modernfuthark <timothys1738@gmail.com>
2020-03-09 17:04:33 -0400

    Created file and inserted code

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - Create a new dog data struct
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ * Return: Pointer to new struct
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *temp;
+
+	temp = malloc(sizeof(dog_t));
+	if (!temp)
+		return (NULL);
+	if (!name || !owner)
+		return (NULL);
+
+	temp->name = name;
+	temp->age = age;
+	temp->owner = owner;
+
+	return (temp);
+}
