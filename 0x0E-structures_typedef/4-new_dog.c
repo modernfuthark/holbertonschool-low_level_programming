d39e35715c3ab119ae16c2649ee01a240861ca2f
modernfuthark <timothys1738@gmail.com>
2020-03-10 09:32:53 -0400

    Updated file to include strdup to properly malloc

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+char *_strdup(char *);
 #include <stdlib.h>
 /**
  * new_dog - Create a new dog data struct
@@ -11,18 +12,55 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *temp;
-	char *sName = name;
-	char *sOwner = owner;
+	char *sName;
+	char *sOwner;
 
 	temp = malloc(sizeof(dog_t));
 	if (!temp)
 		return (NULL);
+
+	sName = _strdup(name);
+	sOwner = _strdup(owner);
 	if (!sName || !sOwner)
+	{
+		free(temp);
+		free(sName);
+		free(sOwner);
 		return (NULL);
+	}
 
 	temp->name = sName;
 	temp->age = age;
 	temp->owner = sOwner;
-
 	return (temp);
+}
+
+/**
+ * _strdup - Duplicates a string
+ * @str: String
+ * Return: Pointer to new string, NULL if malloc failed
+ */
+
+char *_strdup(char *str)
+{
+	char *copy;
+	int i, len = 0;
+
+	if (!str)
+		return (NULL);
+
+	while (str[++len])
+		;
+
+	copy = malloc(sizeof(char) * len + 1);
+	if (!copy)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		copy[i] = str[i];
+	}
+
+	copy[i] = '\0';
+	return (copy);
 }
