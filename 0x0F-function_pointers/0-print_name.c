fff52f524d622aff7a57369d7687cc3e64e15e26
modernfuthark <timothys1738@gmail.com>
2020-03-11 13:30:36 -0400

    Created file and inserted code

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * print_name - Print name using an external function
+ * @name: Name to print
+ * @f: External function
+ * Return: Nothing, function is void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
