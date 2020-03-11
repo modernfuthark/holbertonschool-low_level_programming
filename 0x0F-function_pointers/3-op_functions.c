2c68ce0a9ef04f32aace2ca5b4a0f6bd636a2776
modernfuthark <timothys1738@gmail.com>
2020-03-11 13:54:20 -0400

    Updated task 3 files

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+/**
+ * op_add - Add A B
+ * @a: Number 1
+ * @b: Number 2
+ * Return: A added to B
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtract A B
+ * @a: Number 1
+ * @b: Number 2
+ * Return: A subtraced by B
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiply A B
+ * @a: Number 1
+ * @b: Number 2
+ * Return: A multiplied by B
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divide A B
+ * @a: Number 1
+ * @b: Number 2
+ * Return: A divided by B
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Mod A B
+ * @a: Number 1
+ * @b: Number 2
+ * Return: A mod B
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
