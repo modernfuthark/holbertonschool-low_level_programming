7da15ce0b3647cddd378643cdc0f42955bb3a561
modernfuthark <timothys1738@gmail.com>
2020-03-13 13:51:43 -0400

    Updated betty formatting

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,15 +1,16 @@
 #include "variadic_functions.h"
 /**
- * 
- * 
- * 
+ * print_numbers - Print any number of ints
+ * @separator: Separator between numbers
+ * @n: Number of inputs
+ * Return: Nothing, function is void
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
-	
+
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
 	{
