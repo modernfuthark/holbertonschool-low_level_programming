7e316a00e23dd0695c3a487ef64fb7a5ac2c4472
modernfuthark <timothys1738@gmail.com>
2020-03-19 19:40:25 -0400

    Created file and inserted code

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void tortoise(void) __attribute__((constructor));
+/**
+ * tortoise - Print before the main!
+ * Return: Nothing, function is void
+ */
+
+void tortoise(void)
+{
+	printf("ou're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
