59d8020fdced2880fc625ddbca3174f9ceb34566
modernfuthark <timothys1738@gmail.com>
2020-03-19 19:36:24 -0400

    Created file and inserted code

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - Frees a list
+ * @head: Head of list
+ * Return: Nothing, function is void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+	}
+	free(head);
+}
