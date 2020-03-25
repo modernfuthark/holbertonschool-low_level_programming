1462d4ec124d04bb116937de727fb8fe1f3d6934
modernfuthark <timothys1738@gmail.com>
2020-03-24 20:45:31 -0400

    Created file and inserted code

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * pop_listint - Free the head of a list and return its value
+ * @head: Head of list
+ * Return: Contents of node
+ */
+
+int pop_listint(listint_t **head)
+{
+	int hold;
+	listint_t *newHead;
+
+	if (!head)
+		return (0);
+
+	hold = (*head)->n;
+	newHead = (*head)->next;
+
+	free(*head);
+	*head = newHead;
+
+	return (hold);
+}
