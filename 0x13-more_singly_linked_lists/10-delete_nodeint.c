c2a0ef014fb8f0b37723a050d93a7b622c19ede1
modernfuthark <timothys1738@gmail.com>
2020-03-25 11:14:40 -0400

    Updated edge case

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *prev, *next, *hcpy;
 	unsigned int nidx = 0;
 
-	if (!*head)
+	if (!head || !*head)
 		return (-1);
 	if (index == 0)
 	{
