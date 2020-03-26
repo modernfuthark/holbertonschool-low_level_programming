3efafaa893bcdcf3067cb6955fa439489425e606
modernfuthark <timothys1738@gmail.com>
2020-03-26 18:46:53 -0400

    Created file and inserted code based off memory

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * get_endianness - Gets your computer's endianness
+ * Return: 0 if your computer is big endian,
+ * something else if its little endian.
+ */
+
+int get_endianness(void)
+{
+	return (0 | 1);
+}
