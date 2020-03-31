a37f5bf35ad6ae916fe2734f27de5172ab4199e5
modernfuthark <timothys1738@gmail.com>
2020-03-31 18:35:11 -0400

    Updated betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	while(text_content[len])
+	while (text_content[len])
 		++len;
 
 	writecnt = write(fd, text_content, len);
