c3e8f5e1c19cc5bb4231cb145ea3aec58ba55060
modernfuthark <timothys1738@gmail.com>
2020-05-03 19:37:07 -0400

    Created file and added to directory

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * print_dlistint - Print a doubly linked list's content
+ * @h: Head of list
+ * Return: Number of nodes, -1 on error
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int size;
+	const dlistint_t *temp;
+
+	if (!h)
+		return (-1);
+
+	temp = malloc(sizeof(dlistint_t));
+	if (!temp)
+		return (-1);
+
+	temp = h;
+
+	for (size = 0; temp; size++)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+
+	return (size);
+}
