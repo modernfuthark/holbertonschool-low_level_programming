a57479b8eee4741a3468aa0ff771741fba6ac695
modernfuthark <timothys1738@gmail.com>
2020-05-06 10:50:02 -0400

    This was the source of memory leaks lol

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,22 +8,11 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int size;
-	const dlistint_t *temp;
 
-	if (!h)
-		return (0);
-
-	temp = malloc(sizeof(dlistint_t));
-	if (!temp)
-		return (-1);
-
-	temp = h;
-
-	for (size = 0; temp; size++)
+	for (size = 0; h; size++)
 	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
-
 	return (size);
 }
