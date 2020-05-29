ea55a883297e7f4e0046c11ab95ec91107226a4d
modernfuthark <timothys1738@gmail.com>
2020-05-29 02:49:15 -0400

    [N] Added file to directory for task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table of size size
+ * @size: Size of table to create
+ * Return: Empty hash table, NULL on error
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new;
+	unsigned long int i;
+
+	new = malloc(sizeof(hash_table_t));
+	if (!new)
+		return (NULL);
+
+	new->size = size;
+	new->array = malloc(sizeof(hash_node_t) * size);
+	if (new->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		new->array[i] = NULL;
+	return (new);
+}
