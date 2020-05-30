bb4bc7a957f56c12f07fc15c0b3563720a95d379
modernfuthark <timothys1738@gmail.com>
2020-05-29 21:09:07 -0400

    [+30,31] Added NULL check for temp->value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,6 +27,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(temp->value);
 			temp->value = cval;
+			if (temp->value == NULL)
+				return (0);
 			return (1);
 		}
 		temp = temp->next;
