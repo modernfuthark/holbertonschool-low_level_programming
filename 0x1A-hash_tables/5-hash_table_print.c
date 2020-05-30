1e0f06476618f7bd4822880ddb45bbd96843f5d1
modernfuthark <timothys1738@gmail.com>
2020-05-29 21:34:54 -0400

    [N] Added file to directory

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: Hash table
+ * Return: Nothing, function is void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, j = 0;
+	int flag = 0;
+	hash_node_t *at;
+
+	if (ht)
+	{
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+		{
+			if (ht->array[i])
+			{
+				if (!flag)
+					printf(", ");
+				at = ht->array[i];
+				for (j = 0; at; j++)
+				{
+					printf("'%s': '%s'", at->key, at->value);
+					at = at->next;
+				}
+			}
+			flag = 1;
+		}
+		printf("}\n");
+	}
+}
